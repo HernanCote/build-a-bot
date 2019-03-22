b251c92e27e13c5a4b62ea7b57aac508bcfddf38
Hernán Cote <hernan.cote@outlook.com>
2019-03-22 16:28:46 -0500

    Initial commit

---
+++ src/shared/CollapsibleSection.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="header">
+      <span v-if="open" @click="open = !open">&#x25B2; Collapse</span>
+      <span v-if="!open" @click="open = !open">&#x25BC; Expand</span>
+    </div>
+    <!-- Toggable -->
+    <slot v-if="open"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CollapsibleSection',
+  data() {
+    return {
+      open: true,
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.header {
+  background-color: #bbb;
+  padding: 3px;
+  cursor: pointer;
+
+  span {
+    display: inline-block;
+    width: 100%;
+  }
+}
+</style>
+
